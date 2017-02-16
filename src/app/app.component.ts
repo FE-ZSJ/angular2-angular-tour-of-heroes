adfc979303c4128f8c0e478e7e876a9c053d5d07
FE-ZSJ <sjfly0611@162.com>
2017-02-16 11:28:27 +0800

    主从结构页面展现英雄列表及详情（ngFor，ngIf，事件）

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,103 @@
 import { Component } from '@angular/core';
-export class Hero{
+
+export class Hero{//一个实体类
   id: number;
   name: string;
 }
-@Component({
+
+const HEROES: Hero[] = [//由Hero类的实例构成的数组
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+@Component({ //装饰器
   selector: 'my-app',
-  template: `
-    <h1>{{ title }}</h1>
-    <h2>{{ hero.name }} details!</h2>
-    <div><label>id: </label>{{ hero.id }}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)] = "hero.name" placeholder="name">
+  template: `        <!--模板-->
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+          [class.selected]="hero === selectedHero"
+          (click)="onSelect(hero)"> <!--ngFor,属性绑定,事件-->
+        <span class="badge">{{ hero.id }}</span>{{ hero.name }}
+      </li>
+    </ul>
+    <div *ngIf="selectedHero"> <!--ngIf-->
+      <h1>{{ title }}</h1>
+      <h2>{{ selectedHero.name }} details!</h2>
+      <div>
+        <label>id: </label>{{ selectedHero.id }}
+      </div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)] = "selectedHero.name" placeholder="name"> <!--双向绑定-->
+      </div>
     </div>
-  `
+  `,
+  styles: [` <!--样式-->
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
-export class AppComponent {
+
+export class AppComponent {//组件
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm222'
-  };
+  selectedHero: Hero;
+  heroes = HEROES;
+
+  onSelect(hero: Hero): void {//点击事件
+    this.selectedHero = hero;
+  }
 }
