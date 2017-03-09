f3c68c0b3ff4a3d88b00d45d2c4248981c1d6446
zhusujuan <sjfly0611@126.com>
2017-03-09 11:10:45 +0800

    angular基本构造块-组件

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,6 +1,7 @@
 /**
  * Created by Administrator on 2017/2/22.
  */
+//组件负责屏幕的一小块区域(视图)
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
@@ -12,13 +13,13 @@ import { HeroService } from './hero.service';
   templateUrl: 'dashboard.component.html',//指向新的模板文件
   styleUrls: [ 'dashboard.component.css' ]
 })
-
-export class DashBoardComponent implements OnInit{
+//组件通过一些由属性和方法组成的API与视图交互
+export class DashBoardComponent implements OnInit{//在类中定义组件的应用逻辑，为视图提供支持
   heroes: Hero[] = [] ;
 
   constructor(private heroService: HeroService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {//通过生命周期钩子在组件生命周期的各个时间点插入自己操作
     this.heroService.getHeroes()
       .then(heroes => this.heroes = heroes.slice(1,5));
   }
