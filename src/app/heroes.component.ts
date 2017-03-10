54cbb9b4648331cf149492c33e65ce9bab7126c1
FE-ZSJ <sjfly0611@162.com>
2017-03-10 15:35:45 +0800

    angular基本构造块-元数据

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -4,13 +4,16 @@ import { Router } from '@angular/router';
 import {Hero} from "./hero";//导入用到的Hero类
 import { HeroService } from "./hero.service";
 
-@Component({ //装饰器
-  moduleId: module.id,
-  selector: 'my-heroes',
-  templateUrl: 'heroes.component.html',
+//用装饰器将元数据附加到类上
+@Component({ //装饰器将紧随其后的类标记为组件类
+  //元数据告诉angular获取组件的构建块
+  moduleId: module.id,//为与模块相关的URL提供基地址
+  selector: 'my-heroes',//css选择器，告诉angular在父级HTML中查找<my-heroes>标签，创建并插入该组件
+  templateUrl: 'heroes.component.html',//组件HTML模板的模块相对地址
   styleUrls: [ 'heroes.component.css' ]
+  //provider是组件需要的依赖注入提供商数组，该组件的构造函数需要从服务中获取数据
 })
-
+//模板、元数据、组件共同描绘出视图
 export class HeroesComponent implements OnInit{//组件
   title = 'Tour of Heroes';
   selectedHero: Hero;
