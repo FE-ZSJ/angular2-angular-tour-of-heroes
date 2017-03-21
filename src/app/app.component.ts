3b93322f5106cc03a72234ae6c81950b50a6ce02
FE-ZSJ <sjfly0611@162.com>
2017-03-21 17:26:54 +0800

    显示数据 使用ngFor显示数组属性

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,17 @@
  * Created by zhusujuan on 2017/2/20.
  */
 import { Component } from '@angular/core';
+import {Hero} from "./hero";
 
 @Component({
   selector: 'my-app',
   template: `<!--内联模板-->
-    <h1>{{ title }}</h1>
+    <h1>{{ title }}</h1>  
     <h2>my favorite hero is: {{ myhero }}</h2><!--插值表达式-->
+    <p>Heroes:</p>
+    <ul>
+      <li *ngFor="let hero of testHeroes">{{ hero.detail }}</li>
+    </ul>
     <nav>
       <a routerLink="/dashboard" routerLinkActive="active">DashBoard</a>
       <a routerLink="/heroes" routerLinkActive="active">Heroes</a><!--路由器链接-->
@@ -19,10 +24,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';//变量赋值初始化组件
+
   myhero = 'huge';
   author: string;//利用构造函数声明和初始化属性
+  // testHeroes = ['yangmi', 'huge', 'xiaochen', 'junkai'];//字符串数组
+  testHeroes = [
+    new Hero(1234,'yangmi'),
+    new Hero(1245,'huge'),
+    new Hero(1346,'xiaochen'),
+    new Hero(1565,'junkai')
+  ];
+
 
   constructor() {
     this.author = 'zhusujuan';
+    this.myhero = this.testHeroes[0].detail;
   }
 }
