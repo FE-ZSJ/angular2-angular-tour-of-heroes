a9b82f95389544d0ffebe7fe04a1592086540070
FE-ZSJ <sjfly0611@162.com>
2017-03-21 10:41:49 +0800

    angular基本构造块-根模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-//为 Angular 编译器设置整个应用的关键配置
-import { NgModule }      from '@angular/core';//从库模块中导入NgModule装饰器
+//为 Angular 编译器设置整个应用的关键配置，imports数组中应该只有NgModule类。不要放置其它类型的类。
+import { NgModule } from '@angular/core';//从库模块中导入NgModule装饰器
 import { BrowserModule } from '@angular/platform-browser';//从angular库中导入angular模块
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -18,6 +18,7 @@ import { DashBoardComponent } from './dashboard.component';
 import { HeroSearchComponent } from './hero-search.component';
 import { HeroService } from "./hero.service";
 
+//标记为angular模块类(ngModule类)，接收元数据对象，告诉angular如何编译和启动应用
 @NgModule({//装饰器负责把元数据装饰到类上，接收的是描述模块属性的元数据对象
   imports: [//本模块声明的组件模板需要的类所在的其他模块
     BrowserModule,//应用模块需要BrowserModule的某些素材
@@ -28,7 +29,7 @@ import { HeroService } from "./hero.service";
   ],
   //声明本模块中的视图类(组件、指令、管道)
   declarations: [ //包含了所有由我们创建的并属于应用模块的组件、管道和指令。声明关联模块的任务。
-    AppComponent,
+    AppComponent,//告诉angular哪个组件属于APPModule
     HeroesComponent,
     HeroDetailComponent,
     DashBoardComponent,
@@ -39,7 +40,7 @@ import { HeroService } from "./hero.service";
   providers: [//供每个视图使用，守卫服务
     HeroService
   ],//注入器为服务注册一个提供商，用来创建或返回服务，供应商可以放在根模块，也可以放在组件上
-  //指定应用的主视图(根组件)，其他视图的宿主，只有根模块设置
+  //指定应用的主视图(根组件)，其他视图的宿主，只有根模块设置。angular创建它并插入index.html宿主页面
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_module_1 = require('./app/app.module');
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule); //通过引导根模块来启动应用
+//动态(JiT)编译创建浏览器平台,并引导AppModule
 //# sourceMappingURL=main.js.map
