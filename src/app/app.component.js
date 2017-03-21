ca5984782b9f4be03a044b276a746928c612c90a
zhusujuan <sjfly0611@126.com>
2017-03-21 17:26:54 +0800

    显示数据 使用ngFor显示数组属性

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -12,16 +12,25 @@ var __metadata = (this && this.__metadata) || function (k, v) {
  * Created by zhusujuan on 2017/2/20.
  */
 var core_1 = require('@angular/core');
+var hero_1 = require("./hero");
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes'; //变量赋值初始化组件
         this.myhero = 'huge';
+        // testHeroes = ['yangmi', 'huge', 'xiaochen', 'junkai'];//字符串数组
+        this.testHeroes = [
+            new hero_1.Hero(1234, 'yangmi'),
+            new hero_1.Hero(1245, 'huge'),
+            new hero_1.Hero(1346, 'xiaochen'),
+            new hero_1.Hero(1565, 'junkai')
+        ];
         this.author = 'zhusujuan';
+        this.myhero = this.testHeroes[0].detail;
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<!--\u5185\u8054\u6A21\u677F-->\n    <h1>{{ title }}</h1>\n    <h2>my favorite hero is: {{ myhero }}</h2><!--\u63D2\u503C\u8868\u8FBE\u5F0F-->\n    <nav>\n      <a routerLink=\"/dashboard\" routerLinkActive=\"active\">DashBoard</a>\n      <a routerLink=\"/heroes\" routerLinkActive=\"active\">Heroes</a><!--\u8DEF\u7531\u5668\u94FE\u63A5-->\n    </nav>\n    <router-outlet></router-outlet><!--\u8DEF\u7531\u63D2\u5EA7\u2014\u2014\u628A\u6FC0\u6D3B\u7684\u7EC4\u4EF6\u663E\u793A\u5728\u6B64-->\n    <h5>author: {{ author }}</h5>\n  ",
+            template: "<!--\u5185\u8054\u6A21\u677F-->\n    <h1>{{ title }}</h1>  \n    <h2>my favorite hero is: {{ myhero }}</h2><!--\u63D2\u503C\u8868\u8FBE\u5F0F-->\n    <p>Heroes:</p>\n    <ul>\n      <li *ngFor=\"let hero of testHeroes\">{{ hero.detail }}</li>\n    </ul>\n    <nav>\n      <a routerLink=\"/dashboard\" routerLinkActive=\"active\">DashBoard</a>\n      <a routerLink=\"/heroes\" routerLinkActive=\"active\">Heroes</a><!--\u8DEF\u7531\u5668\u94FE\u63A5-->\n    </nav>\n    <router-outlet></router-outlet><!--\u8DEF\u7531\u63D2\u5EA7\u2014\u2014\u628A\u6FC0\u6D3B\u7684\u7EC4\u4EF6\u663E\u793A\u5728\u6B64-->\n    <h5>author: {{ author }}</h5>\n  ",
             styleUrls: ['app/app.component.css']
         }), 
         __metadata('design:paramtypes', [])
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -4,4 +4,9 @@
 export class Hero{//一个实体类
   id: number;
   name: string;
+
+  constructor(//声明一个构造函数及其类型
+    public uid: number,
+    public detail: string
+  ) {}
 }
